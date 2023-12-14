bdbf466c997c96b3c3f1e73978a3338890caee2c
Ꮥʜꫝʟɪɴɪ <150043650+Shalini77986@users.noreply.github.com>
2023-12-14 20:32:02 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -95,7 +95,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="ꜱᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="ꜱᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-     [
+                 [
             InlineKeyboardButton(
                 text="💕ᴍᴀɪɴᴛᴀɪɴᴇʀ 💕",
                 url=f"tg://openmessage?user_id=6910477574",
