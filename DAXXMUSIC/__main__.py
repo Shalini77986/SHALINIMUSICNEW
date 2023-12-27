c548cf825e33655da1a5b4549eb4cd7732791f8e
Ꮥʜꫝʟɪɴɪ <150043650+Shalini77986@users.noreply.github.com>
2023-12-27 16:45:43 +0530

    Update __main__.py

--- DAXXMUSIC/__main__.py
+++ DAXXMUSIC/__main__.py
@@ -40,17 +40,17 @@ async def init():
     await userbot.start()
     await DAXX.start()
     try:
-        await DAXX.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await DAXX.stream_call("https://telegra.ph/file/5b39f31926e6791b53572.mp4")
     except NoActiveGroupCall:
         LOGGER("DAXXMUSIC").error(
-            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗗𝗔𝗫𝗫 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
+            "ᴘʟᴇᴀsᴇ sᴛᴀʀᴛ ʏᴏᴜʀ ʟᴏɢ ɢʀᴏᴜᴘ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴀɴᴅ ʀᴇsᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ."
         )
         exit()
     except:
         pass
     await DAXX.decorators()
     LOGGER("DAXXMUSIC").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 𝗗𝗔𝗫𝗫☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+        "Cᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴs 🎉 ʏᴏᴜʀ ʙᴏᴛ ʜᴀs ʙᴇᴇɴ sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇᴘʟᴏʏᴇᴅ ✅ \n\n ᴍᴀᴅᴇ ʙʏ sʜᴀʟɪɴɪ."
     )
     await idle()
     await app.stop()
